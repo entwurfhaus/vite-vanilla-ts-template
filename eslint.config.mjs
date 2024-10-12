de4585dcc090b73edce8aa058acc3608da141b3c
Brian Ng <brianngchongeu@gmail.com>
2024-10-12 11:10:50 +1100

    fix: corrected eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,14 @@
-import globals from "globals";
 import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
 import simpleImportSort from "eslint-plugin-simple-import-sort";
+import globals from "globals";
+import tseslint from "typescript-eslint";
 
 export default [
   {
     files: ["**/*.{js,mjs,cjs,ts,tsx}"],
     languageOptions: {
       globals: {
-        ...globals.browser,
+        ...globals.node,
       },
       parser: tseslint.parser,
       parserOptions: {
